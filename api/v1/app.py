481b780bbf0d3c61207db387063fef685730f8ec
Charis <charisadu@gmail.com>
2024-08-07 02:54:21 +0100

    create api

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""The Abnb API"""
+
+from os import getenv
+from models import storage
+from flask import Flask
+
+app = Flask(__name__)
+
+from api.v1.views import app_views
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(
+        host=getenv('HBNB_API_HOST', "0.0.0.0"),
+        port=int(getenv('HBNB_API_PORT', 5000)),
+        threaded=True
+    )
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Index route"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def views():
+    """Returns status in json format"""
+    return jsonify({"status": "OK"})
