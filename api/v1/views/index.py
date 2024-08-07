db41824fff5b1a8782b32cb82eaa671348bf96bb
Charis <charisadu@gmail.com>
2024-08-07 07:59:49 +0100

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,17 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', methods=['GET'])
@@ -13,4 +24,7 @@ def views():
 
 @app_views.route('/stats')
 def count():
-    storage.count()
+    dict = {}
+    for cls in classes:
+        dict[cls.lower()] = storage.count(classes[cls])
+    return jsonify(dict)
